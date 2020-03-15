966b281a2236f4c94fd6778f621a19a39a1a4173
Mijail Rondon <mijail@riseup.net>
2020-03-14 23:06:13 -0500

    Add find update

--- lib/alertas_ec/resolvers/update.ex
+++ lib/alertas_ec/resolvers/update.ex
@@ -4,7 +4,17 @@ defmodule AlertasEc.Resolvers.Update do
   """
   alias AlertasEc.Models.Update
 
-  def list_updates(_parent, _args, _resolution) do
-    {:ok, Update.list_updates()}
+  def list(_parent, _args, _resolution) do
+    {:ok, Update.list()}
+  end
+
+  def find(_parent, %{id: id}, _resolution) do
+    case Update.find(id) do
+      nil ->
+        {:error, "Update with id #{id} not found"}
+
+      update ->
+        {:ok, update}
+    end
   end
 end
