f622900d2fa3ff03566ab0e7fa806f89fb66156d
Mijail Rondon <mijail@riseup.net>
2020-03-21 16:56:26 -0500

    Add update resolver error test

--- test/alertas_ec/resolvers/update_test.exs
+++ test/alertas_ec/resolvers/update_test.exs
@@ -8,7 +8,7 @@ defmodule AlertasEc.Resolvers.UpdateTest do
   @opts Server.init([])
 
   describe "Update resolver" do
-    test "list/0 return a list of alerts" do
+    test "list/0 return a list of updates" do
       update = insert(:update)
 
       query = """
@@ -35,12 +35,37 @@ defmodule AlertasEc.Resolvers.UpdateTest do
       assert conn.resp_body =~ update.source
     end
 
+    test "find/1 should return an error when update id doesn't exist" do
+      id = 1
+
+      conn =
+        :post
+        |> conn("/api", update_query(id))
+        |> Server.call(@opts)
+
+      assert conn.state == :sent
+      assert conn.resp_body =~ "Update with id #{id} not found"
+    end
+
     test "find/1 return an update" do
       update = insert(:update)
 
-      query = """
+      conn =
+        :post
+        |> conn("/api", update_query(update.id))
+        |> Server.call(@opts)
+
+      assert conn.state == :sent
+      assert conn.status == 200
+      assert conn.resp_body =~ update.title
+      assert conn.resp_body =~ update.content
+      assert conn.resp_body =~ update.source
+    end
+
+    def update_query(id) do
+      """
         {
-          update(id: #{update.id}){
+          update(id: #{id}){
             id
             title
             content
@@ -49,17 +74,6 @@ defmodule AlertasEc.Resolvers.UpdateTest do
           }
         }
       """
-
-      conn =
-        :post
-        |> conn("/api", query)
-        |> Server.call(@opts)
-
-      assert conn.state == :sent
-      assert conn.status == 200
-      assert conn.resp_body =~ update.title
-      assert conn.resp_body =~ update.content
-      assert conn.resp_body =~ update.source
     end
   end
 end
