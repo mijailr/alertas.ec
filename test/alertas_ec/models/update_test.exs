2a1dbefec036e993539a98f201e6086c7347cd00
Mijail Rondon <mijail@riseup.net>
2020-03-21 17:20:39 -0500

    Remove unused alert variable

--- test/alertas_ec/models/update_test.exs
+++ test/alertas_ec/models/update_test.exs
@@ -5,8 +5,6 @@ defmodule AlertasEc.Models.UpdateTest do
   alias AlertasEc.Models.Update
 
   test "A valid alert" do
-    alert = build(:alert)
-
     assert {:ok, %Update{}} =
              params_with_assocs(:update)
              |> Update.create()
