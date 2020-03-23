63f4753db81c8b6f5e786ef363287ac2b3495811
Mijail Rondon <mijail@riseup.net>
2020-03-23 01:30:57 -0500

    Remove process tables migration

--- priv/repo/migrations/20200323053225_move_index.exs
+++ priv/repo/migrations/20200323053225_move_index.exs
@@ -5,8 +5,6 @@ defmodule AlertasEc.Repo.Migrations.MoveIndex do
   alias AlertasEc.Repo
 
   def change do
-    reindex_updates()
-
     alter table(:updates) do
       remove(:alert_id)
     end
@@ -23,51 +21,7 @@ defmodule AlertasEc.Repo.Migrations.MoveIndex do
     execute("ALTER TABLE updates ADD PRIMARY KEY (id);")
 
     execute(
-      "ALTER TABLE updates ADD CONSTRAINT updates_alert_id_fkey FOREIGN KEY (alert_id) REFERENCES alerts(id);
-"
+      "ALTER TABLE updates ADD CONSTRAINT updates_alert_id_fkey FOREIGN KEY (alert_id) REFERENCES alerts(id);"
     )
-  end
-
-  defp reindex_updates do
-    Alert
-    |> Repo.all()
-    |> Enum.each(&add_uuid/1)
-  end
-
-  defp add_uuid(alert) do
-    from(u in Update, where: u.alert_id == ^alert.id)
-    |> Repo.update_all(set: [alert_uuid: alert.uuid])
-  end
-end
-
-defmodule Update do
-  use Ecto.Schema
-
-  schema("alerts") do
-    field(:title, :string)
-    field(:content, :string)
-    field(:date, :naive_datetime)
-    field(:source, :string)
-    field(:alert_uuid, :binary)
-    belongs_to(:alert, Alert)
-
-    timestamps()
-  end
-end
-
-defmodule Alert do
-  use Ecto.Schema
-
-  schema("alerts") do
-    field(:title, :string)
-    field(:description, :string)
-    field(:date, :naive_datetime)
-    field(:type, :string)
-    field(:uuid, :binary)
-    field(:status, StatusEnum)
-    field(:severity, SeverityEnum)
-    has_many(:updates, Update)
-
-    timestamps()
   end
 end
