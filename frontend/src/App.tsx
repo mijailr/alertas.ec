500ac6822aeb82443109ca7a0c3c5f97f41279dd
Mijail Rondon <mijail@riseup.net>
2020-03-22 23:42:13 -0500

    Add default routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
 import { Grid } from 'react-foundation'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  RouteComponentProps
+} from "react-router-dom";
 import Header from './Components/Header'
 import './App.scss';
 import { ApolloProvider } from '@apollo/react-hooks'
 import Client from './Service/Client'
 import AlertList from './Components/AlertList'
 
+import AlertShow from './Components/AlertShow';
+import { AlertSingle } from './Service/types';
+
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={Client}>
-        <div className="App grid-container">
-          <Header />
-            <Grid>
-              <AlertList />
-            </Grid>
-        </div>
+        <Router>
+          <div className="App grid-container">
+            <Header />
+            <Switch>
+              <Route exact path="/" children={<AlertList />} />
+              <Route path="/alert/:id" children={(props: RouteComponentProps<AlertSingle>) => (
+                <AlertShow id={props.match.params.id} />
+              )} />
+            </Switch>
+          </div>
+        </Router>
       </ApolloProvider>
     );
   }
