49753745bb3d7ca26028c3da8662db539daf1aae
Mijail Rondon <mijail@riseup.net>
2020-03-23 14:19:49 -0500

    Add uuid on production environments

--- config/releases.exs
+++ config/releases.exs
@@ -1,5 +1,8 @@
 import Config
-config :alertas_ec, AlertasEc.Repo, url: System.get_env("DATABASE_URL")
+
+config :alertas_ec, AlertasEc.Repo,
+  url: System.get_env("DATABASE_URL"),
+  migration_primary_key: [name: :id, type: :binary_id]
 
 config :alertas_ec,
   port: String.to_integer(System.get_env("PORT"))
