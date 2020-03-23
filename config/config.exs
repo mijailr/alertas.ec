5a6e75286e2d2f9db89725a30a57daf55dd6be6b
Mijail Rondon <mijail@riseup.net>
2020-03-23 14:08:07 -0500

    Change migrations to have UUID

--- config/config.exs
+++ config/config.exs
@@ -4,4 +4,8 @@ config :alertas_ec,
   port: 3000,
   ecto_repos: [AlertasEc.Repo]
 
+config :alertas_ec,
+       AlertasEc.Repo,
+       migration_primary_key: [name: :id, type: :binary_id]
+
 import_config("#{Mix.env()}.exs")
